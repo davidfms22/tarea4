b7a9c35a4254d38ca18e4af0c40551316fca1083
davidfms22 <david.martinez@greentechmail.com>
2016-03-14 09:59:49 -0500

    todo9

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,19 @@
-import java.sql.*;
-import java.util.HashMap;
+import static spark.Spark.get;
+import static spark.SparkBase.port;
+import static spark.SparkBase.staticFileLocation;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import static spark.Spark.*;
-import spark.template.freemarker.FreeMarkerEngine;
-import spark.ModelAndView;
-import static spark.Spark.get;
-
 import com.heroku.sdk.jdbc.DatabaseUrl;
+
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
 
 public class Main {
 
